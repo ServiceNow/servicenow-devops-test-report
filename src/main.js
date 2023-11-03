95ee6bffbd00baacd7b3952abc62c21f1c1aa79f
kunal.gangaher <kunal.gangaher@servicenow.com>
2023-11-03 16:32:45 +0530

    STRY56331515: Correcting stack trace in logs.

--- src/main.js
+++ src/main.js
@@ -190,7 +190,7 @@ const axios = require('axios');
         } else {
             core.setFailed(`ServiceNow Test Results are NOT created. Please check ServiceNow logs for more details.`);
             core.setFailed(`[ServiceNow DevOps] Test Results, Error message :${e.message}`);
-            core.setFailed(`[ServiceNow DevOps] Test Results, Error message :${err.stack}`);
+            core.setFailed(`[ServiceNow DevOps] Test Results, Error message :${e.stack}`);
         }
     }
     
